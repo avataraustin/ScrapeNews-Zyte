ed5cef9f70bd15a3b4b1631591079e590d779d4d
avataraustin <39737076+avataraustin@users.noreply.github.com>
2023-07-14 13:35:46 +0000

    updated to use zyte api, tested for successful scraping and saving.
    updated to use zyte api for traffic, tested to be sure data was being scraped and saved.

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import scrapy
 import time
 import random
 import csv
+import os
+import scrapy_zyte_api
 
-#Running this script will scrape top news from apnews and store in csv (will take a while to do this) It may also result in ip blacklisting by the site being scraped.
+'''
+This version was adjusted until it yielded proper data, may need further adjustment to store properly in csv and also possible that news site will change tags rendering selector code obsolete until reupdated. 
+'''
+
+# ZYTE_API_KEY = os.environ['ZYTE_API_KEY']
+
+
 
 #import the CrawlerProcess for following links
 from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
 
-
-# link to current 'top news': https://apnews.com/hub/ap-top-news
-# links are in an "a" tag where a class contains "Component 
-# heading" and the href is then the link of the article
-# Example: <a class="Component-headline-0-2-91" data-key="card-headline" href="/article/2024-republicans-subway-chokehold-death-be39b29eef4529bf996c13ec5bcd22ef"><h2 class="Component-heading-0-2-92 Component-headingMobile-0-2-93 -cardHeading undefined">2024 Republican hopefuls rush to defend Marine who put NYC subway rider in fatal chokehold</h2></a>
 
 def scrape_top_news():
   '''
@@ -27,52 +31,73 @@ def scrape_top_news():
   
   top_news = "https://apnews.com/hub/ap-top-news"
   #holder lists for packaging later 
+  
   news_story_links = []
   news_titles = []
   news_story_body = []
   
   class NewsScraper(scrapy.Spider):
     name = "news_scraper"
-  
+  ###test
     def start_requests(self):
-      yield scrapy.Request(url = top_news, callback = self.parse1)
+      yield scrapy.Request(
+        url = top_news,
+        meta={
+          "zyte_api_automap": True,
+        },
+        callback = self.parse1)
   
     #first parsing method to gather the links
     def parse1(self, response):
-      top_news_links = response.css('a[class*=Component-headline]::attr(href)').extract()    
-      #print(top_news_links) 
-      # -> this above code stores the links in 
-      # the format of '/article/tiktok-ban-montana-lawsuit-72be560de89fb87e3c677c8e0cfd9fec' for example.
-      # so the parent domain portion would need to be added
-      #"https://apnews.com" + the link string stored from above.
-
-      #scrape only the first 10 top news links
-      for url in top_news_links[0:10]:
-        time.sleep(random.randint(8,35)) #random time between requests to hopefully avoid labeling as a bot
-        # appending to a list to work with later
-        news_story_links.append("https://apnews.com"+url)
-        yield response.follow(url = ("https://apnews.com"+url), callback = self.parse2) #keep an eye on this, not sure if can do
+      ##### NEED TO ADJUST SELECTORS BELOW TO PROPERLY CHOOSE ON WEBSITE
+      top_news_links = response.css('a[href^="https://apnews.com/article"]::attr(href)').getall()
+     
+      #scrape only the first 5 top news links
+      for story in top_news_links[0:5]: #was called url
         
+        # appending to a list to work with later
+        news_story_links.append(story)
+        yield response.follow(url = story, callback = self.parse2)
           
     # Second parsing method to process the next level in 
     def parse2(self, response):
       article_title = response.css('h1::text').extract()
-      time.sleep(random.randint(5,20)) #random time between
+      #time.sleep(random.randint(5,20)) #random time between
       news_titles.append(article_title) #storing title to work with
       news_body = response.css('body p::text').extract()
-      time.sleep(random.randint(5,20)) #random time between
+      #time.sleep(random.randint(5,20)) #random time between
       news_story_body.append(news_body)
    
-        
-  
+  custom_settings = {
+    'ZYTE_API_KEY': os.environ['ZYTE_API_KEY'],
+    'DOWNLOAD_HANDLERS': {
+      "http": "scrapy_zyte_api.ScrapyZyteAPIDownloadHandler",
+      "https": "scrapy_zyte_api.ScrapyZyteAPIDownloadHandler",
+    },
+    'DOWNLOADER_MIDDLEWARES': {
+      "scrapy_zyte_api.ScrapyZyteAPIDownloaderMiddleware": 1000,
+    },
+    'REQUEST_FINGERPRINTER_CLASS':  "scrapy_zyte_api.ScrapyZyteAPIRequestFingerprinter",
+    'TWISTED_REACTOR': "twisted.internet.asyncioreactor.AsyncioSelectorReactor",
+    #'ZYTE_API_TRANSPARENT_MODE': True,
+    'DOWNLOAD_DELAY': 5.5
+  }      
+
+
+  settings = Settings(custom_settings)
   #run the spider:
-  process = CrawlerProcess()
+  process = CrawlerProcess(settings)
   process.crawl(NewsScraper)
   process.start()
   
+  #print values for debugging
+  print(news_story_links)
+  print(news_titles)
+  print(news_story_body)
+
   
   #zipping together the stored lists of link,title,body
-  zipped_news = zip(news_story_links,news_titles,news_story_body)
+  zipped_news = zip(news_story_links, news_titles, news_story_body)
   
   #store in a gathered data in csv:
   with open('stored_articles.csv', 'w', newline='') as f:
@@ -82,4 +107,3 @@ def scrape_top_news():
 
 if __name__ == "__main__":
   scrape_top_news()
-  
